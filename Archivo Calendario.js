257244df6a42ef4ad7e5ac68bba0c3478d618095
Esanpons <ecs200@hotmail.com>
2023-06-28 15:49:48 +0200

    Segir Aciendo el Ful Calendar

--- Archivo Calendario.js
+++ Archivo Calendario.js
@@ -23,27 +23,27 @@ var Eventos =
         {
             title: 'Nivel 3',
             start: '2022-11-11',
-            mes: ''
+            mes: 'Noviembre'
         },
         {
-            title: 'Nivel 1',
-            start: '2022-10-28',
-            mes: ''
+            title: 'Nivel 2',
+            start: '2022-11-14',
+            mes: 'Noviembre'
         },
         {
-            title: 'Nivel 1',
-            start: '2022-10-28',
-            mes: ''
+            title: 'Nivel 5',
+            start: '2022-11-17',
+            mes: 'Noviembre'
         },
         {
-            title: 'Nivel 1',
-            start: '2022-10-28',
-            mes: ''
+            title: 'Nivel 3',
+            start: '2022-12-03',
+            mes: 'Diciembre'
         },
         {
-            title: 'Nivel 1',
-            start: '2022-10-28',
-            mes: ''
+            title: 'Nivel 3',
+            start: '2022-12-15',
+            mes: 'Diciembre'
         },
         {
             title: 'Nivel 1',
